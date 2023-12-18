75ae98eab098d7d4c686d66ce056250e1beeec99
Leonid Korotkov <leonid.korotkov@vitech.team>
2023-12-18 19:13:42 +0300

    HW2 done

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,54 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
+
+const EmptyRune = -1
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	if input == "" {
+		return input, nil
+	}
+
+	var builder strings.Builder
+	var prev rune = EmptyRune
+	var escaped bool
+
+	for _, ch := range input {
+		if !escaped && string(prev) == "\\" {
+			if string(ch) == "\\" {
+				escaped = true
+			}
+
+			prev = ch
+			continue
+		}
+
+		if count, err := strconv.Atoi(string(ch)); err == nil {
+			if prev == EmptyRune {
+				return input, ErrInvalidString
+			}
+
+			builder.WriteString(strings.Repeat(string(prev), count))
+			prev = EmptyRune
+		} else {
+			if prev != EmptyRune {
+				builder.WriteRune(prev)
+			}
+
+			prev = ch
+		}
+
+		escaped = false
+	}
+
+	if prev != EmptyRune {
+		builder.WriteRune(prev)
+	}
+
+	return builder.String(), nil
 }
